d21c4b4fb7577928b9f695f2376098a993390600
cxntered <cxntered@duck.com>
2023-11-09 03:27:24 +1100

    📦 NEW: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+import "./globals.css";
+
+const fontSans = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "Quartz",
+	description: "A link shortener for the modern web",
+	openGraph: {
+		title: "Quartz",
+		description: "A link shortener for the modern web",
+		url: "https://cxntered.dev/",
+		siteName: "q.cxntered.dev",
+		images: [
+			{
+				url: "https://cxntered.me/assets/Quartz.png"
+			}
+		],
+		locale: "en_US",
+		type: "website"
+	}
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en">
+			<body className={cn(
+				"min-h-screen bg-background font-sans antialiased",
+				fontSans.className
+			)}>
+				{children}
+			</body>
+		</html>
+	);
+}
