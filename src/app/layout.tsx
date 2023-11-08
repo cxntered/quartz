da11756c8dd7cdb883e4d9a76a0c0020b4929fe8
cxntered <cxntered@duck.com>
2023-11-09 03:34:01 +1100

    👌 IMPROVE: extend fontfamily in tailwind config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
-import "./globals.css";
+import "./styles/globals.css";
 
-const fontSans = Inter({ subsets: ["latin"] });
+export const fontSans = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Quartz",
