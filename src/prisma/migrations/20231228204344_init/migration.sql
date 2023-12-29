2e70c71402b39cb57c4a7229e9d84d3b6e3fa933
cxntered <cxntered@duck.com>
2023-12-30 10:03:00 +1100

    👌 IMPROVE: database updates

--- src/prisma/migrations/20231228204344_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "link" (
-    "url" TEXT NOT NULL,
-    "id" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "link_id_key" ON "link"("id");
---
+++ src/prisma/migrations/20231229214807_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "link" (
+    "url" TEXT NOT NULL,
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "link_pkey" PRIMARY KEY ("id")
+);
