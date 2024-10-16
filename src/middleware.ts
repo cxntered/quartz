6a2f20b5be506134cb501aec3542675a4abcd5c3
cxntered <cxntered@duck.com>
2024-10-16 23:04:32 +1100

    ✨ feature: add ratelimiting currently 10 urls in 60s, might change later

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,56 @@
 import { NextRequest, NextResponse } from "next/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+const ratelimit = new Ratelimit({
+	redis: Redis.fromEnv(),
+	limiter: Ratelimit.slidingWindow(10, "60 s")
+});
 
 export const config = {
 	matcher: [
 		/*
 		 * Match all request paths except for the ones starting with:
-		 * - api (API routes)
 		 * - _next/static (static files)
 		 * - _next/image (image optimization files)
 		 * - favicon.ico (favicon file)
 		 */
-		"/((?!api|_next/static|_next/image|favicon.ico).*)"
+		"/((?!_next/static|_next/image|favicon.ico).*)"
 	]
 };
 
 export async function middleware(req: NextRequest) {
 	if (req.nextUrl.pathname === "/") return;
 
-	const res = await fetch(
-		`${req.nextUrl.origin}/api/get?url=${req.nextUrl.pathname.split("/")[1]}`
-	);
+	if (req.nextUrl.pathname.startsWith("/api")) {
+		if (req.nextUrl.pathname !== "/api/create") return;
 
-	if (res.status === 404) return;
+		const ip = req.ip ?? "127.0.0.1";
+		const { success, pending, limit, reset, remaining } = await ratelimit.limit(ip);
 
-	const query = await res.json();
-	const url = new URL(query.url);
+		return success
+			? NextResponse.next()
+			: NextResponse.json(
+					{
+						message: "Rate limit exceeded! Try again in a few minutes.",
+						ok: false,
+						limit,
+						remaining,
+						reset,
+						pending
+					},
+					{ status: 429 }
+			  );
+	} else {
+		const res = await fetch(
+			`${req.nextUrl.origin}/api/get?url=${req.nextUrl.pathname.split("/")[1]}`
+		);
 
-	return NextResponse.redirect(url);
+		if (res.status === 404) return;
+
+		const query = await res.json();
+		const url = new URL(query.url);
+
+		return NextResponse.redirect(url);
+	}
 }
